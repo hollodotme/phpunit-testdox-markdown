9249175dad1be2ac9178b0e77441105aeded4e6a
Holger Woltersdorf <hw@hollo.me>
2018-05-20 15:10:19 +0200

    Add interface for markdown file writer

---
+++ src/Interfaces/WritesMarkdownFile.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace hollodotme\PHPUnit\TestListeners\TestDox\Interfaces;
+
+interface WritesMarkdownFile
+{
+	public function writeLegend() : void;
+
+	public function writeHeadline( string $title, int $level ) : void;
+
+	public function writeCheckbox( string $label, bool $ticked ) : void;
+
+	public function writeBulletListing( array $elements, int $indentLevel = 0 ) : void;
+
+	public function writeBlockQuoteListing( array $elements, int $indentLevel = 0 ) : void;
+
+	public function writeHorizontalRule() : void;
+
+	public function getFilePath() : string;
+
+	public function close() : void;
+}
